b2a8a8cb334bd509bd12f832f762c2d2b12834ce
Chase Moody <cmoody06@gmail.com>
2014-11-11 21:16:58 -0600

    Fragment rendering for things

---
+++ javascripts/app/who/thing.js
@@ -0,0 +1,20 @@
+define(function(require) {
+	"use strict";
+
+	// Libs
+	var $ = require('jquery');
+	var Backbone = require('backbone');
+
+	// Template
+    var tpl = require('text!app/who/tpl/thing.html');
+    var template = _.template(tpl);
+
+	return Backbone.View.extend({	
+		render: function() {
+		    this.$el.html(template(this.model.toJSON()));
+		    
+		    return this;
+		}
+	});
+
+});
--- javascripts/app/who/who.js
+++ javascripts/app/who/who.js
@@ -5,19 +5,44 @@ define(function(require) {
 	var $ = require('jquery');
 	var Backbone = require('backbone');
 
+	// Models/Collections
+	var Things = require('app/who/collection/things');
+
 	// Template
     var tpl = require('text!app/who/tpl/who.html');
     var template = _.template(tpl);
+
+    // SubViews
+    var ThingView = require('app/who/thing');
+
+    // var tplThing = require('text!app/who/tpl/thing.html');
+    // var templateThing = _.template(tplThing);
+
+    var frag = document.createDocumentFragment();
 	
 	return Backbone.View.extend({
 		className: 'who',
 
 		initialize: function() {
-			this.render();
+			Things.Collection.fetch({reset:true});
+
+			Things.Collection.on('reset', this.render, this);
 		},
 
 		render: function() {
 			this.$el.html(template());
+
+			Things.Collection.each(function(thing) {
+
+				var x = new ThingView({
+		        	model: thing
+		        });
+
+		        frag.appendChild(x.render().el);
+
+			}, this);
+
+			this.$el.append(frag);
 			
 			return this;
 		}
