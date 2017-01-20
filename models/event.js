8a9c6e3b388bb693f19394eeaac1ab98964dd09a
vernistage <sdickers10@gmail.com>
2017-01-20 14:32:32 -0600

    resolve dependencies issues

--- models/event.js
+++ models/event.js
@@ -1,12 +1,10 @@
 // Namespace the app
 EventModel = Backbone.Model.extend({
-
   initialize: function(){
   }
-
 });
 
 $(document).ready(function(){
   var event = new EventModel({title: "best"})
-  alert("the event titles is" + event.get(title) + "!")
+  alert("the event title is" + event.get(title) + "!")
 });
