e80f7a8a000d492107645a947be327636efaa6c2
vernistage <sdickers10@gmail.com>
2017-01-20 21:16:27 -0600

    add today and tomorrow collection of events

--- models/event.js
+++ models/event.js
@@ -5,18 +5,22 @@ EventModel = Backbone.Model.extend({
     "begin_time":"<p>DESCRIPTION TBD</p>",
     "venue": "TBD",
     "image": null
-  },
-  initialize: function(){
   }
 });
 
-EventCollection = Backbone.Collection.extend({
-  model: EventModel
+TodayCollection = Backbone.Collection.extend({
+  model: EventModel,
+  urlRoot: "http://do512.com/events/today.json"
+});
+
+TomorrowCollection = Backbone.Collection.extend({
+  model: EventModel,
+  urlRoot: "http://do512.com/events/tomorrow.json"
 });
 
 $(document).ready(function(){
   var event = new EventModel();
-  var today_events = new EventCollection([]);
-  var tom_events = new EventCollection([]);
+  var today_events = new TodayCollection([]);
+  var tom_events = new TomorrowCollection([]);
   console.log(today_events.size())
 });
