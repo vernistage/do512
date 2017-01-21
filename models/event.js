c31c56170f7683b879c8bcbb771dfb921fb08535
vernistage <sdickers10@gmail.com>
2017-01-20 23:22:39 -0600

    fetching data correctly... now to implement view

--- models/event.js
+++ models/event.js
@@ -1,26 +1,68 @@
-// Namespace the app
+// Model
 EventModel = Backbone.Model.extend({
-  defaults: {
-    "title": "TBD",
-    "begin_time":"<p>DESCRIPTION TBD</p>",
-    "venue": "TBD",
-    "image": null
-  }
+  // defaults: {
+  // }
 });
 
+var EventView = Backbone.View.extend({
+  el: '#container',
+
+  initialize: function(){
+    this.render();
+  },
+
+  render: function(){
+    this.$el.html(this.model.get('title'))
+  }
+
+});
+
+// Collections
 TodayCollection = Backbone.Collection.extend({
   model: EventModel,
-  urlRoot: "http://do512.com/events/today.json"
+  url: "http://do512.com/events/today.json",
+  sync : function(method, collection, options) {
+    options.dataType = "jsonp";
+    return Backbone.sync(method, collection, options);
+  }
 });
 
 TomorrowCollection = Backbone.Collection.extend({
   model: EventModel,
-  urlRoot: "http://do512.com/events/tomorrow.json"
+  url: "http://do512.com/events/tomorrow.json"
 });
 
+var TodayView = Backbone.View.extend({
+  el: '#container',
+  collection: TodayCollection,
+
+  initialize: function(options){
+    this.collection = options.collection;
+    this.render()
+  },
+
+  render: function(){
+    console.log(this);
+  }
+})
+
+// Driver code
+
 $(document).ready(function(){
-  var event = new EventModel();
-  var today_events = new TodayCollection([]);
-  var tom_events = new TomorrowCollection([]);
-  console.log(today_events.size())
+  var todays_events = new TodayCollection();
+  var data
+  todays_events.fetch({
+    dataType: 'jsonp',
+    success: function(collection, response, options){
+      data = response.events;
+    }
+  })
+  var tomorrows_events = new TomorrowCollection();
+  tomorrows_events.fetch({
+    dataType: 'jsonp'
+  })
+
+  var test = new EventModel({title: "bonjour"})
+  var view = new EventView({model: test})
+  var eventer = new TodayView({collection: data});
 });
