a61aaeeb15b5e6143c4a4dce26fd5bc2742c98c1
vernistage <sdickers10@gmail.com>
2017-01-20 20:54:49 -0600

    introduce backbone style collections

--- models/event.js
+++ models/event.js
@@ -1,10 +1,21 @@
 // Namespace the app
 EventModel = Backbone.Model.extend({
+  defaults: {
+    "title": "TBD",
+    "begin_time":"<p>DESCRIPTION TBD</p>",
+    "venue": "TBD",
+    "image": null
+  },
   initialize: function(){
   }
 });
 
+EventCollection = Backbone.Collection.extend({
+  model: EventModel
+});
+
 $(document).ready(function(){
-  var event = new EventModel({name: "best"});
-  alert("hello" + event.get("name"));
+  var event = new EventModel();
+  var today_events = new EventCollection([]);
+  var tom_events = new EventCollection([]);
 });
